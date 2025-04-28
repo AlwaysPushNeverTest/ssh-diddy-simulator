95f1dd673f872fbc237aeb69c1863c6e917b7d7b
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-04-28 01:43:37 -0400

    [task] hello world

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/gliderlabs/ssh"
+)
+
+var port string
+
+func main() {
+	ssh.Handle(func(s ssh.Session) {
+		io.WriteString(s, "Hello world\n")
+	})
+	fmt.Println(os.Getenv("PORT"))
+	if port = os.Getenv("PORT"); port == "" {
+		port = "8080"
+	}
+	log.Fatal(ssh.ListenAndServe(":"+port, nil))
+}
